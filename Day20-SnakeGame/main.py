c5012a6fe9ef9d90e1f462684bb022216e6da358
Rahul Kumar <kumarrahul771996@gmail.com>
2023-04-21 19:18:26 +0530

    snake game (not completed)

---
+++ Day20-SnakeGame/main.py
@@ -0,0 +1,25 @@
+from turtle import Screen
+import time
+from snake import Snake
+
+screen = Screen()
+# s.setup(width="600", height="600")
+screen.bgcolor("black")
+screen.tracer(0)
+
+snake = Snake()
+
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+	screen.update()
+	time.sleep(0.1)
+
+	snake.move()
+	
+# s.exitonclick()
